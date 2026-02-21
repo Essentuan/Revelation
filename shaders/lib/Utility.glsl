89f826c6f76699cfdc2269d49d80fa9b8df7a9da
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-02-21 11:07:38 +0800

    Half float support

--- shaders/lib/Utility.glsl
+++ shaders/lib/Utility.glsl
@@ -12,6 +12,7 @@
 
 #include "/settings.glsl"
 
+#include "/lib/utility/Compat.glsl"
 #include "/lib/utility/Math.glsl"
 #include "/lib/utility/Matrix.glsl"
 #include "/lib/utility/Pack.glsl"
@@ -19,9 +20,8 @@
 #include "/lib/utility/Interpolate.glsl"
 #include "/lib/utility/Phase.glsl"
 #include "/lib/utility/SH.glsl"
-#include "/lib/utility/Load.glsl"
 #include "/lib/utility/Offset.glsl"
-#include "/lib/utility/SubgroupOps.glsl"
+#include "/lib/utility/Load.glsl"
 
 //================================================================================================//
 
