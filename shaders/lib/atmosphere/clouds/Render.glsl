d1447cb90e75890f0c62f602addc3d51d5287223
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-03-29 22:27:01 +0800

    Fix moon light

--- shaders/lib/atmosphere/clouds/Render.glsl
+++ shaders/lib/atmosphere/clouds/Render.glsl
@@ -338,11 +338,10 @@ void CompositeClouds(inout vec3 skyRadiance, in vec4 cloudData, in vec3 rayDir) 
 	if (cloudData.w < 1.0) {
 		vec3 cloudPos = atmosphereViewPos + rayDir * cloudData.z;
 
-		// Compute irradiance
-        vec3 sunIrradiance = sunIrradiance * AtmosphereTransmittanceToSun(cloudPos, worldSunDir);
-        vec3 moonIrradiance = sunIrradiance * AtmosphereTransmittanceToSun(cloudPos, -worldSunDir) * moonlightMult;
-
-		vec3 directIlluminance = 128.0 * (sunIrradiance + moonIrradiance);
+		// Compute illumination to clouds
+        vec3 sunIlluminance = sunIrradiance * AtmosphereTransmittanceToSun(cloudPos, worldSunDir);
+        vec3 moonIlluminance = sunIrradiance * AtmosphereTransmittanceToSun(cloudPos, -worldSunDir) * moonlightMult;
+		vec3 directIlluminance = 128.0 * (sunIlluminance + moonIlluminance);
 
 		// Normalized height in clouds
 		float heightFraction = saturate((length(cloudPos) - cumulusBottomRadius) * rcp(cumulusThickness));
