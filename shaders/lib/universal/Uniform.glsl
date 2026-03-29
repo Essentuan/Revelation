ff41547cdbc79f3d483e4f118caa3e18d126b0fd
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-03-29 13:56:52 +0800

    Remove old atmos lut 1

--- shaders/lib/universal/Uniform.glsl
+++ shaders/lib/universal/Uniform.glsl
@@ -22,10 +22,6 @@ uniform sampler2D depthtex1;
 uniform sampler2D depthtex2;
 
 // Custom samplers
-uniform sampler3D scatteringLut;
-uniform sampler2D transmittanceLut;
-uniform sampler2D irradianceLut;
-
 uniform sampler2D tLutTex;
 uniform sampler2D msLutTex;
 uniform sampler2D skyViewTex;
