3f3dff88a00026b7e719248ebfa0017d417fe818
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-04-12 13:15:43 +0800

    Fix AgX Minimal color transform

--- shaders/lib/post/AgX.glsl
+++ shaders/lib/post/AgX.glsl
@@ -73,16 +73,17 @@ vec3 agxDefaultContrastApprox_7th(vec3 x) {
 }
 
 vec3 agx(vec3 val) {
-    const mat3 agx_mat = mat3(
-    0.842479062253094, 0.0423282422610123, 0.0423756549057051,
-    0.0784335999999992,  0.878468636469772,  0.0784336,
-    0.0792237451477643, 0.0791661274605434, 0.879142973793104);
+    const mat3 agx_inset_matrix = mat3(
+        0.856627153315983, 0.137318972929847, 0.11189821299995,
+        0.0951212405381588, 0.761241990602591, 0.0767994186031903,
+        0.0482516061458583, 0.101439036467562, 0.811302368396859
+    );
 
     // const float min_ev = -12.47393f;
     // const float max_ev = 4.026069f;
 
     // Input transform (inset)
-    val = agx_mat * val;
+    val = agx_inset_matrix * val;
 
     // Log2 space encoding
     val = clamp(log2(val * rcp(middle_grey)), min_ev, max_ev);
@@ -95,13 +96,14 @@ vec3 agx(vec3 val) {
 }
 
 vec3 agxEotf(vec3 val) {
-    const mat3 agx_mat_inv = mat3(
-    1.19687900512017, -0.0528968517574562, -0.0529716355144438,
-    -0.0980208811401368, 1.15190312990417, -0.0980434501171241,
-    -0.0990297440797205, -0.0989611768448433, 1.15107367264116);
+    const mat3 agx_inverse_outset_matrix = mat3(
+        1.1271005818144366432, -0.14132976349843826565, -0.14132976349843824772,
+        -0.1106066430966032116, 1.1578237022162717623, -0.11060664309660291788,
+        -0.016493938717834568157, -0.01649393871783425265, 1.2519364065950402828
+    );
 
     // Inverse input transform (outset)
-    val = agx_mat_inv * val;
+    val = agx_inverse_outset_matrix * val;
 
     // sRGB IEC 61966-2-1 2.2 Exponent Reference EOTF Display
     // NOTE: We're linearizing the output here. Comment/adjust when
