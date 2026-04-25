8f4488c5dc78c7ab31c0c33b3e0567e5a154e56a
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-04-25 20:10:17 +0800

    Half to full res scene reproj

--- shaders/lib/lighting/SSILVB.glsl
+++ shaders/lib/lighting/SSILVB.glsl
@@ -271,7 +271,7 @@ vec4 CalculateSSILVB(vec2 fragCoord, vec3 viewPos, vec3 worldNormal, float skyli
                 if (sampleOccludedBit > 0u) {
                     // vec3 sampleNormal = mat3(gbufferModelView) * FetchSurfaceNormal(sampleTexel);
 
-                    vec3 sampleRadiance = texelFetch(colortex4, sampleTexel >> 1, 0).rgb;
+                    vec3 sampleRadiance = texelFetch(colortex4, sampleTexel, 0).rgb;
                     sampleRadiance = min(sampleRadiance, 512.0); // clamp to prevent overflow
                     irradiance.rgb += float(bitCount(sampleOccludedBit)) *
                         // fastSqrtNR0(saturate(-dot(sampleNormal, sampleDirFront))) *
