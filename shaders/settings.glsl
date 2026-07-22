434a666389ad8df6cc3d3699c9c2d813232f9a6c
HaringPro <132074996+HaringPro@users.noreply.github.com>
2026-07-22 17:17:34 +0800

    Improve refraction via Newton’s method

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -201,7 +201,7 @@ const float realShadowMapRes = float(shadowMapResolution) * MC_SHADOW_QUALITY;
 
 /* Refractions */
 	// #define RAYTRACED_REFRACTION
-	#define REFRACTION_STRENGTH 1.0 // [0.0 0.1 0.2 0.25 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.5 6.0 6.5 7.0 7.5 8.0 9.5 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0]
+    #define REFRACTION_SAMPLES 8 // [1 2 4 8 16 32 64 128]
 
 /* Emissive */
 	#define EMISSIVE_MODE 1 // [0 1 2]
