df7ae8ddee5b604806d8623b8a5cabc1a1dd5bcf
Essentuan <51426319+Essentuan@users.noreply.github.com>
2026-09-11 10:55:42 -0500

    Misc changes

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -162,7 +162,7 @@ const float realShadowMapRes = float(shadowMapResolution) * MC_SHADOW_QUALITY;
     const vec3 ircSizeHalf = vec3(ircSizeHalfInt);
 
     #define DIFFUSE_SPR_SAMPLES 4 // [0 1 2 3 4]
-    #define DIFFUSE_SPR_RADIUS 32 // [4 8 12 16 20 24 28 32]
+    #define DIFFUSE_SPR_RADIUS 32 // [4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64]
     #define DIFFUSE_HISTORY_LENGTH 32 // [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64]
     #define DIFFUSE_DENOISER_PASSES 5 // [0 1 2 3 4 5 6 7]
 
